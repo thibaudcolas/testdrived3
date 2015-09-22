5008a019da19b5b1dd44c2e8fbec69468514f226
Thibaud Colas <thibaudcolas@gmail.com>
2015-09-23 02:23:10 +1200

    Add run function for the first exercise

--- exercises/hello_world/exercise.js
+++ exercises/hello_world/exercise.js
@@ -1,3 +1,14 @@
 import runner from '../../lib/runner';
 
-export default runner();
+function runFunction(helloWorld) {
+    console.log(`\nnumbers = [${[4, 8, 15]}]`);
+    console.log(`helloWorld(numbers) = ${helloWorld([4, 8, 15])}`);
+
+    console.log(`\nnumbers = [${[-100, 5, 0]}]`);
+    console.log(`helloWorld(numbers) = ${helloWorld([-100, 5, 0])}`);
+
+    console.log(`\nnumbers = [${[9, 9, 9]}]`);
+    console.log(`helloWorld(numbers) = ${helloWorld([9, 9, 9])}`);
+}
+
+export default runner(runFunction);

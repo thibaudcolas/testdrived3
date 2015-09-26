8172b98c59e1bf9a79280647e05fc78e44e01a2d
Thibaud Colas <thibaudcolas@gmail.com>
2015-09-27 11:37:23 +1300

    Fix wording error

--- exercises/chaining_methods/exercise.js
+++ exercises/chaining_methods/exercise.js
@@ -3,7 +3,7 @@ import runner from '../../lib/runner';
 function runFunction(submission) {
     submission();
 
-    console.log(`\npage HTML: ${global.document.body.outerHTML}`);
+    console.log(`\nHTML page: ${global.document.body.outerHTML}`);
 }
 
 export default runner(runFunction);

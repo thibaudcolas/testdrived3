0d951cda48c629ad055d90e3eb6738c171dbb17d
Thibaud Colas <thibaudcolas@gmail.com>
2015-10-05 05:04:05 +1300

    Add test for manual chart

---
+++ src/tests/part_1_coding_a_chart_manually.spec.js
@@ -0,0 +1,5 @@
+require('../lib/runner');
+
+global.submission = require('../exercises/part_1_coding_a_chart_manually/solution/solution');
+
+require('../exercises/part_1_coding_a_chart_manually/spec');

60e702264ad80e1b5273d510e20378809aad70c3
Thibaud Colas <thibaudcolas@gmail.com>
2015-11-16 16:36:48 +0200

    Transition to jsdom 7 & drop node 0.12 support

--- src/lib/html-document.js
+++ src/lib/html-document.js
@@ -1,5 +1,5 @@
 import jsdom from 'jsdom';
 
 // Create a JSDOM document that we'll play with during the exercises.
-global.document  = jsdom.jsdom('<html><body><div class="html-chart"></div></body></html>');
-global.window    = global.document.parentWindow;
+global.document = jsdom.jsdom('<html><body><div class="html-chart"></div></body></html>');
+global.window = global.document.defaultView;

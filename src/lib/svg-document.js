ad063aea2d71792d3c3b87e623f4a6e57246cbf4
Thibaud Colas <thibaudcolas@gmail.com>
2015-11-16 16:39:02 +0200

    Finish transition & fix failing tests

--- src/lib/svg-document.js
+++ src/lib/svg-document.js
@@ -2,4 +2,4 @@ import jsdom from 'jsdom';
 
 // Create a JSDOM document that we'll play with during the exercises.
 global.document  = jsdom.jsdom('<html><body><svg class="chart"></svg></body></html>');
-global.window    = global.document.parentWindow;
+global.window = global.document.defaultView;

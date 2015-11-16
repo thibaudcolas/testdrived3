db8b08a2589575b54bf7b0607a821c162b570946
Thibaud Colas <thibaudcolas@gmail.com>
2015-11-16 16:43:09 +0200

    Harden tests

--- tests.sh
+++ tests.sh
@@ -6,34 +6,34 @@ $TESTDRIVED3 list
 
 $TESTDRIVED3 help
 
-$TESTDRIVED3 select "Hello, World!"
+$TESTDRIVED3 select 'Hello, World!'
 $TESTDRIVED3 run es5/exercises/hello_world/solution/solution.js
 $TESTDRIVED3 verify es5/exercises/hello_world/solution/solution.js
 
-$TESTDRIVED3 select "Selecting an element"
+$TESTDRIVED3 select 'Selecting an element'
 $TESTDRIVED3 run es5/exercises/selecting_an_element/solution/solution.js
 $TESTDRIVED3 verify es5/exercises/selecting_an_element/solution/solution.js
 
-$TESTDRIVED3 select "Chaining methods"
+$TESTDRIVED3 select 'Chaining methods'
 $TESTDRIVED3 run es5/exercises/chaining_methods/solution/solution.js
 $TESTDRIVED3 verify es5/exercises/chaining_methods/solution/solution.js
 
-$TESTDRIVED3 select "Part 1: Coding a chart, manually"
+$TESTDRIVED3 select 'Part 1: Coding a chart, manually'
 $TESTDRIVED3 run es5/exercises/part_1_coding_a_chart_manually/solution/solution.js
 $TESTDRIVED3 verify es5/exercises/part_1_coding_a_chart_manually/solution/solution.js
 
-$TESTDRIVED3 select "Part 1: Coding a chart, automatically"
+$TESTDRIVED3 select 'Part 1: Coding a chart, automatically'
 $TESTDRIVED3 run es5/exercises/part_1_coding_a_chart_automatically/solution/solution.js
 $TESTDRIVED3 verify es5/exercises/part_1_coding_a_chart_automatically/solution/solution.js
 
-$TESTDRIVED3 select "Part 1: Scaling to fit"
+$TESTDRIVED3 select 'Part 1: Scaling to fit'
 $TESTDRIVED3 run es5/exercises/part_1_scaling_to_fit/solution/solution.js
 $TESTDRIVED3 verify es5/exercises/part_1_scaling_to_fit/solution/solution.js
 
-$TESTDRIVED3 select "Part 2: Coding a chart, manually"
+$TESTDRIVED3 select 'Part 2: Coding a chart, manually'
 $TESTDRIVED3 run es5/exercises/part_2_coding_a_chart_manually/solution/solution.js
 $TESTDRIVED3 verify es5/exercises/part_2_coding_a_chart_manually/solution/solution.js
 
-$TESTDRIVED3 select "Part 2: Coding a chart, automatically"
+$TESTDRIVED3 select 'Part 2: Coding a chart, automatically'
 $TESTDRIVED3 run es5/exercises/part_2_coding_a_chart_automatically/solution/solution.js
 $TESTDRIVED3 verify es5/exercises/part_2_coding_a_chart_automatically/solution/solution.js

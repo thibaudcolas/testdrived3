5f3e62fac5039eb2278aaeb811b8d9d5fb4986f0
Thibaud Colas <thibaudcolas@gmail.com>
2015-12-22 15:49:27 +0200

    First working solution to part3 rotating columns

--- src/exercises/part_3_rotating_into_columns/problem.md
+++ src/exercises/part_3_rotating_into_columns/problem.md
@@ -1,5 +1,7 @@
 In the previous parts of this tutorial we made a basic bar chart in HTML and then in SVG; now, we’ll improve the display by rotating the chart into columns and adding axes. We’ll also switch to a real dataset, showing the relative frequency of letters in the English language.
 
+TODO Rotating into columns
+
 ----------------------------------------------------------------------
 
 ## HINTS
--- src/exercises/part_3_rotating_into_columns/solution/solution.js
+++ src/exercises/part_3_rotating_into_columns/solution/solution.js
@@ -3,5 +3,35 @@
 var d3 = require('d3');
 
 module.exports = function(frequencies) {
-    // TODO
+    // CHANGE chart size variables
+    var chartWidth = 960;
+    var chartHeight = 500;
+    var barWidth = chartWidth / frequencies.length;
+
+    // CHANGE widthScale -> heightScale
+    var heightScale = d3.scale.linear()
+        .domain([0, d3.max(frequencies, function(d) { return d.value; })])
+        .range([chartHeight, 0]);
+
+    var chart = d3.select('.chart')
+        .attr('width', chartWidth)
+        .attr('height', chartHeight);
+
+    // CHANGE translating on width
+    var bar = chart.selectAll('g')
+        .data(frequencies)
+    .enter().append('g')
+        .attr('transform', function(d, i) { return 'translate(' + (i * barWidth) + ', 0)'; });
+
+    // CHANGE define height & y instead of width.
+    bar.append('rect')
+        .attr('y', function(d) { return heightScale(d.value); })
+        .attr('height', function(d) { return chartHeight - heightScale(d.value); })
+        .attr('width', barWidth - 1);
+
+    bar.append('text')
+        .attr('x', barWidth / 2)
+        .attr('y', function(d) { return heightScale(d.value) + 3; })
+        .attr('dy', '.75em')
+        .text(function(d) { return d.value; });
 };

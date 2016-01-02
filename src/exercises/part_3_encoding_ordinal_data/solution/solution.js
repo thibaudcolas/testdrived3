0f1d94bc637909f8c75cffa93b8143a378a55c02
Thibaud Colas <thibaudcolas@gmail.com>
2016-01-02 19:21:34 +0100

    Add solution to latest exercise

--- src/exercises/part_3_encoding_ordinal_data/solution/solution.js
+++ src/exercises/part_3_encoding_ordinal_data/solution/solution.js
@@ -5,7 +5,10 @@ var d3 = require('d3');
 module.exports = function(frequencies) {
     var chartWidth = 960;
     var chartHeight = 500;
-    var barWidth = chartWidth / frequencies.length;
+
+    var widthScale = d3.scale.ordinal()
+        .domain(frequencies.map(function(d) { return d.letter; }))
+        .rangeRoundBands([0, chartWidth], .1);
 
     // The range now goes from the height of the chart to 0, since SVG coordinates start at the top.
     var heightScale = d3.scale.linear()
@@ -20,15 +23,15 @@ module.exports = function(frequencies) {
     var bar = chart.selectAll('g')
         .data(frequencies)
     .enter().append('g')
-        .attr('transform', function(d, i) { return 'translate(' + (i * barWidth) + ', 0)'; });
+        .attr('transform', function(d, i) { return 'translate(' + widthScale(d.letter) + ', 0)'; });
 
     bar.append('rect')
         .attr('y', function(d) { return heightScale(d.value); })
         .attr('height', function(d) { return chartHeight - heightScale(d.value); })
-        .attr('width', barWidth - 1);
+        .attr('width', widthScale.rangeBand());
 
     bar.append('text')
-        .attr('x', barWidth / 2)
+        .attr('x', widthScale.rangeBand() / 2)
         .attr('y', function(d) { return heightScale(d.value) + 3; })
         .attr('dy', '.75em')
         .text(function(d) { return d.value; });
